63aeb39d7aaf2569b97c411c22c8479ed8f056fc
Fran <gallosino@hotmail.com>
2018-03-15 16:33:23 +0100

    Primer commit y push

---
+++ src/main/java/Ejercicio/PersonaFiscal.java
@@ -0,0 +1,13 @@
+package Ejercicio;
+
+public class PersonaFiscal {
+	private String nombre;
+	private int edad;
+	private int sueldo;
+
+	public PersonaFiscal(String _nombre, int _edad, int _sueldo) {
+		this.nombre = _nombre;
+		this.edad = _edad;
+		this.sueldo = _sueldo;
+	}
+}
---
+++ src/main/java/Ejercicio/TipoImpuesto.java
@@ -0,0 +1,11 @@
+package Ejercicio;
+
+public class TipoImpuesto {
+	private int porcentaje;
+	private int porcentaje30;
+	private String nombre;
+
+	public TipoImpuesto(int _porcentaje, int _porcentaje30, String _nombre) {
+
+	}
+}
